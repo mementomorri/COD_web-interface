a52e750ab2cf7dbd4229a6d5fa3cca2480eb04f5
mementomorri <44745297+mementomorri@users.noreply.github.com>
2021-10-01 12:49:00 +0600

    Delete COD directory

--- COD/wwwroot/js/hatch_decorate.js
+++
@@ -1,7 +0,0 @@
-﻿setTimeout(angle, 1000);
-function angle() {
-    element = document.getElementById('T_street_rot3');
-    trans = element.style.transform;
-    console.log(trans)
-    console.log(element.style)
-}
